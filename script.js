b8dce3b8f7439a3813fee54160700913bad04271
Andrew Bui <andrew.bui.au@gmail.com>
2021-01-03 20:21:02 +1100

    added initial logic functionality including display output of numbers as well as operators

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-let $newDiv = $('<p>');
-$newDiv.text("Hello World");
-let $body = $('body');
-$body.append($newDiv);
+const $numbers = $('.number');
+const $operators = $('.operator')
+const $numberAreaOne = $('.numberAreaOne');
+const $numberAreaTwo = $('.numberAreaTwo');
+const $operatorArea = $('.operatorArea');
+
+let numberAreaOneArray = [];
+let operatorValue = '';
+
+// Number Buttons Event Listener
+$numbers.on('click', (event) => {
+    event.preventDefault();
+
+    if (numberAreaOneArray.length > 10) {
+        return
+    } else {
+        numberAreaOneArray.push(event.target.value);
+    }
+
+    if (operatorValue === '') {
+        $numberAreaOne.text(numberAreaOneArray.join(''));
+    } else {
+        $numberAreaTwo.text(numberAreaOneArray.join(''));
+    }
+});
+
+
+
+// Operator Buttons Event Listener
+$operators.on('click', (event) => {
+    event.preventDefault();
+    $operatorArea.html(event.target.value);
+    console.log($operatorArea);
+});
+
+
+
+
