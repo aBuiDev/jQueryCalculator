8e5880a5725277508c87dfd938233dca67bce59a
Andrew Bui <andrew.bui.au@gmail.com>
2021-01-03 21:40:04 +1100

    finalised project logic

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ const $operatorArea = $('.operatorArea');
 const $equal = $('.equal');
 const $clear = $('.clear');
 
+const $outputDisplay = $('.outputDisplay');
+
 let numberAreaOneArray = [];
 let numberOne = null;
 let numberAreaTwoArray = [];
 let numberTwo = null;
 let operatorValue = '';
+let output = null;
 
 
 
@@ -48,7 +51,7 @@ $operators.on('click', (event) => {
 
 
 
-// Calculate Button
+// Clear Button
 $clear.on('click', (event) => {
     event.preventDefault();
     numberAreaOneArray = [];
@@ -56,11 +59,93 @@ $clear.on('click', (event) => {
     numberAreaTwoArray = [];
     numberTwo = null;
     operatorValue = '';
+    output = null;
     $numberAreaOne.text(numberOne);
     $numberAreaTwo.text(numberTwo);
     $operatorArea.text(operatorValue);
-
+    $outputDisplay.text(output);
 });
+
+
+
+// Calculator Delegator
+$equal.on('click', (event) => {
+    event.preventDefault();
+    if (operatorValue === '+') {
+        add();
+    } else if (operatorValue === '-') {
+        subtract();
+    } else if (operatorValue === '/') {
+        divide();
+    } else if (operatorValue === 'x') {
+        multiply();
+    } else if (operatorValue === '**') {
+        square();
+    } else if (operatorValue === '%') {
+        percentage();
+    } else if (operatorValue === 'mod') {
+        modulo();
+    }
+});
+
+
+
+// Calculator Function (Add)
+const add = () => {
+    output = (parseFloat(numberOne) + parseFloat(numberTwo));
+    $outputDisplay.text(output);
+}
+
+
+
+// Calculator Function (Subtract)
+const subtract = () => {
+    output = (parseFloat(numberOne) - parseFloat(numberTwo));
+    $outputDisplay.text(output);
+}
+
+
+
+// Calculator Function (Divide)
+const divide = () => {
+    output = (parseFloat(numberOne) / parseFloat(numberTwo));
+    $outputDisplay.text(output);
+}
+
+
+
+// Calculator Function (Multiply)
+const multiply = () => {
+    output = (parseFloat(numberOne) * parseFloat(numberTwo));
+    $outputDisplay.text(output);
+}
+
+
+
+// Calculator Function (Square)
+const square = () => {
+    output = (parseFloat(numberOne) ** parseFloat(numberTwo));
+    $outputDisplay.text(output);
+}
+
+
+
+// Calculator Function (Percentage)
+const percentage = () => {
+    output = ((parseFloat(numberOne) / parseFloat(numberTwo)) * 100);
+    $outputDisplay.text(output);
+}
+
+
+
+// Calculator Function (Modulo)
+const modulo = () => {
+    output = (parseFloat(numberOne) % parseFloat(numberTwo));
+    $outputDisplay.text(output);
+}
+
+
+
 
 
 
